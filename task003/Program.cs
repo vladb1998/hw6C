73645e23c1b24c50b5c7d9323ef0602252dfff55
vlad <vladbelyvskij@gmail.com>
2022-08-24 13:20:16 +0300

    Напишите программу, которая будет преобразовывать десятичное число в двоичное.

---
+++ task003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите k1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = "";
+
+while (num > 0)
+{
+    if(num % 2 != 0) result += "1";
+    else result += "0";
+    num /= 2;
+}
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write(result[result.Length - 1 - i]);
+}
