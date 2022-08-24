4b87665aa0d9893f4c2b1c667deebb91ba835f85
vladb1998 <110394305+vladb1998@users.noreply.github.com>
2022-08-24 14:38:19 +0300

    Update Program.cs
    Задание 2

--- task002/Program.cs
+++ task002/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите k1: ");
+Console.Write("Введите k1: ");
 double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
@@ -7,11 +7,12 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
  
- 
+ if(k1 != k2)
+{
 double x = -(b1 - b2) / (k1 - k2);
 double y = k1 * x + b1;
- 
 x = Math.Round(x, 2);
 y = Math.Round(y, 2);
- 
 Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
+else {Console.WriteLine("Заданные прямые не пересекаются!");}
